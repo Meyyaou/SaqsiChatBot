109d86ef06ac263f6fdc143e0a2d857e65516d4f
mey yaou <meynedjmadehbiyayaou@gmail.com>
2024-01-15 00:01:27 +0100

    signin added

--- app/src/main/java/com/example/saqsi/LoginActivity.java
+++ app/src/main/java/com/example/saqsi/LoginActivity.java
@@ -1,11 +1,19 @@
 package com.example.saqsi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
@@ -16,7 +24,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView signin= findViewById(R.id.signupText);
+        SpannableString spannableString = new SpannableString("Not registered yet? Sign up here");
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Intent intent = new Intent(LoginActivity.this, SigninActivity.class);
+                startActivity(intent);
+            }
+        };
+        spannableString.setSpan(clickableSpan, 20, spannableString.length(), 0);
+        spannableString.setSpan(new ForegroundColorSpan(Color.WHITE), 20, spannableString.length(), 0);
+        // Set the SpannableString to the TextView
+        signin.setText(spannableString);
 
+        // Enable the movement method for the TextView to make the link clickable
+        signin.setMovementMethod(LinkMovementMethod.getInstance());
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginButton);
---
+++ app/src/main/java/com/example/saqsi/SigninActivity.java
@@ -0,0 +1,35 @@
+package com.example.saqsi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SigninActivity extends AppCompatActivity {
+    EditText username;
+    EditText email;
+    EditText password;
+    Button signinButton;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        email = findViewById(R.id.email);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        signinButton = findViewById(R.id.signinButton);
+
+        signinButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                    Toast.makeText(SigninActivity.this, "Signin done successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
